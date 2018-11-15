66f7993ada6edff91133164b5dc35ec16c3c39ff
Igor Giovannini <igor.giovannini@post.ch>
2018-11-15 23:14:04 +0100

    Removed key annotation

--- Translawesome/Models/Translation.cs
+++ Translawesome/Models/Translation.cs
@@ -4,9 +4,7 @@ namespace ch.igorgiovannini.Translawesome.Models
 {
     public class Translation
     {
-        [Key]
         public string Language { get; set; }
-        [Key]
         public string Key { get; set; }
         public string Value { get; set; }
     }
